c0f29ca44e6a6d762998db358278f762d378a4c1
Dell <Dell@DESKTOP-703RS57>
2022-07-29 17:39:06 +0530

    cmdk

---
+++ src/test/java/pack1/GIt123.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class GIt123 {
+
+	public static void main(String[] args) {
+		System.out.println("git 12233 class run");
+
+	}
+
+}
---
+++ src/test/java/pack1/GIt124.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class GIt124 {
+
+	public static void main(String[] args) {
+		System.out.println("git 1223333444 class run");
+
+	}
+
+}
