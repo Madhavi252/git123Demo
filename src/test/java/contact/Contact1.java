e86a5961eaa828458406f60b431a3df233cdacab
Dell <Dell@DESKTOP-703RS57>
2022-07-29 17:47:43 +0530

    vdh

---
+++ src/test/java/contact/Contact1.java
@@ -0,0 +1,9 @@
+package contact;
+
+public class Contact1 {
+
+	public static void main(String[] args) {
+		System.out.println("Contact123 is created");
+	}
+
+}
---
+++ src/test/java/contact/Contact2.java
@@ -0,0 +1,9 @@
+package contact;
+
+public class Contact2 {
+
+	public static void main(String[] args) {
+		System.out.println("Contact223 is created");
+	}
+
+}
